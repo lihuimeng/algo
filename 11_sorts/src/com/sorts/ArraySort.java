e1012a6ade7756e64494201c55f5c7f4f4237f04
huimeng.li <275448736@qq.com>
2020-10-26 15:42:36 +0800

    冒泡排序优化

--- 11_sorts/src/com/sorts/ArraySort.java
+++ 11_sorts/src/com/sorts/ArraySort.java
@@ -9,6 +9,7 @@ public class ArraySort {
 
     /**
      * 冒泡排序
+     *
      * @param array
      * @param size
      */
@@ -16,18 +17,24 @@ public class ArraySort {
         if (size < 2) {
             return;
         }
+        int count = 0;
         for (int i = 0; i < size; i++) {
             boolean flag = false;
             for (int j = 0; j < size - i - 1; j++) {
+                count++;
                 if (array[j] > array[j + 1]) {
-                    swap(array, j, j+1);
-                    flag = true;
+                    swap(array, j, j + 1);
+                    //如果是第一个元素交换了，逆序已经被修正没有了，所以就不用了循环了
+                    if (j != 0) {
+                        flag = true;
+                    }
                 }
             }
             if (!flag) {
                 break;
             }
         }
+        System.out.println("循环次数：" + count);
     }
 
     private static void swap(int[] array, int index1, int index2) {
@@ -37,7 +44,7 @@ public class ArraySort {
     }
 
     public static void main(String[] args) {
-        int[] ints = {8, 6, 5, 4, 3, 2, 1, 5};
+        int[] ints = {3, 2, 1, 4, 5, 6, 7, 8};
         bubbleSort(ints, 8);
         System.out.println("123");
     }
